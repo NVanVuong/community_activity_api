5d68a07c89d86c0fe662db46301f06135ad347d2
Nguyễn Văn Vương <93960884+NVanVuong@users.noreply.github.com>
2024-05-28 23:06:22 +0700

    Update create-proof.dto.ts

---
+++ src/module/proof/dto/create-proof.dto.ts
@@ -0,0 +1,25 @@
+import { IsOptional } from 'class-validator';
+
+export class CreateProofDto {
+  name: string;
+  description: string;
+
+  @IsOptional()
+  image: string;
+
+  // Case create proof for external activity
+  @IsOptional()
+  startDate: Date;
+
+  @IsOptional()
+  endDate: Date;
+
+  @IsOptional()
+  address: string;
+
+  @IsOptional()
+  score: number;
+
+  @IsOptional()
+  subcategoryId: string;
+}
